55bb98110d3ceb8d06262b66c865650179fdabfe
sahilraj87083 <sahilraj87093@gmail.com>
2026-01-10 01:08:48 +0530

    feat(questions): add get question by id with ownership and soft delete checks

--- Backend/src/controllers/question.controller.js
+++ Backend/src/controllers/question.controller.js
@@ -2,7 +2,7 @@ import { Question } from "../models/question.model";
 import {asyncHandler} from '../utils/AsyncHandler.utils.js'
 import {ApiError} from '../utils/ApiError.utils.js'
 import {ApiResponse} from '../utils/ApiResponse.utils.js'
-import mongoose from 'mongoose'
+import mongoose, { isValidObjectId } from 'mongoose'
 import {validationResult} from 'express-validator'
 import {normalizeUrlservice, uploadQuestionService} from '../services/question.services.js'
 
@@ -36,7 +36,23 @@ const getAllQuestions = asyncHandler(async (req, res) => {
 })
 
 const getQuestionById = asyncHandler(async (req, res) => {
+    const {questionId} = req.params
 
+    if(!isValidObjectId(questionId)){
+        throw new ApiError(400, "Invalid question ID");
+    }
+
+    const question = await Question.findOne({
+        _id : questionId,
+        ownerId : req.user._id,
+        isDeleted : false
+    })
+
+    if (!question) {
+        throw new ApiError(404, "Question not found");
+    }
+
+    return res.status(200).json(new ApiResponse(200, "Question fetched", question))
 })
 
 const updateQuestion = asyncHandler(async (req, res) => {
