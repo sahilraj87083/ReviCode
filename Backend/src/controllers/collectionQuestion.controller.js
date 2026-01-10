8d1634da3815fbe6cae6d150e66f2ae7fd06ebab
sahilraj87083 <sahilraj87093@gmail.com>
2026-01-11 02:28:28 +0530

    feat(collections): add question to collection with ownership and duplicate checks

--- Backend/src/controllers/collectionQuestion.controller.js
+++ Backend/src/controllers/collectionQuestion.controller.js
@@ -3,10 +3,60 @@ import {ApiError} from '../utils/ApiError.utils.js'
 import {ApiResponse} from '../utils/ApiResponse.utils.js'
 import { isValidObjectId } from 'mongoose'
 import {CollectionQuestion} from '../models/collectionQuestion.model.js'
+import { validateCollection } from '../services/collectionQuestion.service.js'
+import { Question } from '../models/question.model.js'
+import { Collection } from '../models/collection.model.js'
 
 
 const addQuestionToCollection = asyncHandler( async (req, res) => {
-    
+
+    const { collectionId } = req.params;
+    const { questionId } = req.body;
+
+    if(!isValidObjectId(questionId)){
+        throw new ApiError(400, "Invalid question ID");
+    }
+
+    await validateCollection(collectionId, req.user._id);
+
+    const question = await Question.findOne({
+        _id: questionId,
+        ownerId: req.user._id,
+        isDeleted: false,
+    });
+
+    if (!question) {
+        throw new ApiError(404, "Question not found");
+    }
+
+    try {
+        await Promise.all([
+            CollectionQuestion.create({
+                collectionId,
+                questionId
+            }),
+
+            Collection.updateOne(
+                { _id : collectionId},
+                {$inc : {
+                    questionsCount : 1
+                }}
+            )
+        ])
+        
+    } catch (error) {
+
+        if (error.code === 11000) {
+            throw new ApiError(409, "Question already in this collection");
+        }
+
+        throw error;
+    }
+
+    return res
+        .status(201)
+        .json(new ApiResponse(201, "Question added to collection"));
+
 })
 
 const removeQuestionFromCollection = asyncHandler( async (req, res) => {
--- Backend/src/services/collectionQuestion.service.js
+++ Backend/src/services/collectionQuestion.service.js
@@ -1,8 +1,27 @@
+import { isValidObjectId } from 'mongoose'
 import {CollectionQuestion} from '../models/collectionQuestion.model.js'
 import { ApiError } from '../utils/ApiError.utils.js'
+import {Collection} from '../models/collection.model.js'
+import {Question} from '../models/question.model.js'
 
 const validateCollection =  async (collectionId, userId) => {
-    
+
+    if(!isValidObjectId(collectionId)){
+        throw new ApiError(400, "Invalid collection ID");
+    }
+
+    const collection = await Collection.findOne(
+        {
+            _id : collectionId,
+            ownerId : userId
+        }
+    )
+
+    if (!collection) {
+        throw new ApiError(404, "Collection not found");
+    }
+
+    return collection;
 }
 
 
